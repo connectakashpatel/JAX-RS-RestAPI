2efccd3ce9614d88b3380097e94f4492623f4501
Akash <Akash@DESKTOP-O0P1UFR.fios-router.home>
2018-03-10 22:50:07 -0500

    Update

--- RestAPI_JaxRs/src/Controller/PersonCtrl.java
+++ RestAPI_JaxRs/src/Controller/PersonCtrl.java
@@ -1,18 +1,46 @@
 package Controller;
 
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import DAO.PersonDAO;
+import Model.Person;
 
-
+@Path("/rest")
 public class PersonCtrl {
-
 	
-	public static void main(String args[]){
+	Person p;
+	PersonDAO personDAO = new PersonDAO();
+	
+
+	@Path("{id}")
+	@GET
+	@Produces("application/json")
+	public Response showuser(@PathParam("id") int id) {
 		
-		PersonDAO personDAO = new PersonDAO();
+		JSONObject jobj = new JSONObject();
+		p = personDAO.showUsers(id);
 		
-		personDAO.connect();
+		try {
+			
+			jobj.put("id", p.getId());
+			jobj.put("name", p.getName());
+			
+			
+		} catch (JSONException e) {
+			
+			e.printStackTrace();
+		}
+		
+		return Response.status(200).entity(jobj).build();
 		
 	}
-	
 	
 }
--- RestAPI_JaxRs/src/DAO/PersonDAO.java
+++ RestAPI_JaxRs/src/DAO/PersonDAO.java
@@ -2,15 +2,43 @@ package DAO;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import Model.Person;
 
 public class PersonDAO{
 	
-	public void addUser(Person p) {
+	Person p = new Person();
+	
+	public Person showUsers(int id) {
 		
 		Connection c = connect();
+		String sql = "SELECT * FROM person WHERE id=?";
+		PreparedStatement ps;
+		ResultSet rs;
+		
+		try {
+			
+			ps = c.prepareStatement(sql);
+			ps.setInt(1, id);
+			rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				
+				p.setId(rs.getInt(0));
+				p.setName(rs.getString("name"));
+				
+			}
+			
+		
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return p;
+		
 	}
 
 
