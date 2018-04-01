61b653df33bba9d6921da0bb9940f951f15c6d52
Akash Patel <connectakashpatel@gmail.com>
2018-04-01 00:39:14 -0400

    Fixed 101 ticket

--- RestAPI_JaxRs/src/Model/Person.java
+++ RestAPI_JaxRs/src/Model/Person.java
@@ -5,6 +5,20 @@ public class Person {
 	private String name;
 	private int id;
 	
+	
+	// KP Example
+	private double age;
+
+	public double getAge() {
+		return age;
+	}
+	public void setAge(double age) {
+		this.age = age;
+	}
+	
+	// *********************************
+	
+	
 	public String getName() {
 		return name;
 	}
